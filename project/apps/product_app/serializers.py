a2240e785b9dac3f3cd508d51f934c06cf29a573
MrWhiteres <alexandr.leluk.99@gmail.com>
2022-11-03 15:22:32 +0200

    Init DRF - app, base settings + first work with rest

--- project/apps/product_app/serializers.py
+++ project/apps/product_app/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
+from rest_framework.fields import ImageField, ListField
+from rest_framework.serializers import ModelSerializer, Serializer
 
 from .models import Product, ImageProduct
 
@@ -7,10 +7,27 @@ from .models import Product, ImageProduct
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ["title", 'price', 'created_date', 'update_date', 'owner']
 
 
 class ImageSerializer(ModelSerializer):
     class Meta:
         model = ImageProduct
-        fields = "__all__"
+        fields = ["image", 'product']
+
+
+class MultipleImageSerializer(Serializer):
+    images = ListField(
+        child=ImageField()
+    )
+
+
+class FullSerializer(ModelSerializer):
+    image = ImageSerializer()
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def create(self, validated_data):
+        pass
